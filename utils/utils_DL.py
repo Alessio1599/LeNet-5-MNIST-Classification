47c085350f1f3dfdca1143687544053eb60fdf41
Alessio Guarachi <99431824+Alessio1599@users.noreply.github.com>
2024-07-06 14:49:01 +0200

    Add files via upload

---
+++ utils/utils_DL.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_history(history,metric=None):
+  fig, ax1 = plt.subplots(figsize=(10, 8))
+
+  epoch_count=len(history.history['loss'])
+
+  line1,=ax1.plot(range(1,epoch_count+1),history.history['loss'],label='train_loss',color='orange')
+  ax1.plot(range(1,epoch_count+1),history.history['val_loss'],label='val_loss',color = line1.get_color(), linestyle = '--')
+  ax1.set_xlim([1,epoch_count])
+  ax1.set_ylim([0, max(max(history.history['loss']),max(history.history['val_loss']))])
+  ax1.set_ylabel('loss',color = line1.get_color())
+  ax1.tick_params(axis='y', labelcolor=line1.get_color())
+  ax1.set_xlabel('Epochs')
+  _=ax1.legend(loc='lower left')
+
+  if (metric!=None):
+    ax2 = ax1.twinx()
+    line2,=ax2.plot(range(1,epoch_count+1),history.history[metric],label='train_'+metric)
+    ax2.plot(range(1,epoch_count+1),history.history['val_'+metric],label='val_'+metric,color = line2.get_color(), linestyle = '--')
+    ax2.set_ylim([0, max(max(history.history[metric]),max(history.history['val_'+metric]))])
+    ax2.set_ylabel(metric,color=line2.get_color())
+    ax2.tick_params(axis='y', labelcolor=line2.get_color())
+    _=ax2.legend(loc='upper right')
+
+def show_confusion_matrix(conf_matrix,class_names,figsize=(10,10)):
+  fig, ax = plt.subplots(figsize=figsize)
+  img=ax.matshow(conf_matrix)
+  tick_marks = np.arange(len(class_names))
+  _=plt.xticks(tick_marks, class_names,rotation=45)
+  _=plt.yticks(tick_marks, class_names)
+  _=plt.ylabel('Real')
+  _=plt.xlabel('Predicted')
+
+  for i in range(len(class_names)):
+    for j in range(len(class_names)):
+        text = ax.text(j, i, '{0:.1%}'.format(conf_matrix[i, j]),
+                       ha='center', va='center', color='w')
