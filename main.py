c21635d0e30b900d7df5729a241cf6a2295f0a9e
fateme karimi <168193063+fatemekarimi1227@users.noreply.github.com>
2024-07-07 21:51:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
-
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
