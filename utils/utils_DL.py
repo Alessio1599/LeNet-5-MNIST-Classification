4a05e68c61e09b4693422c23c4f1b78ede9f18a1
Alessio1599 <alessio.guarachi@studio.unibo.it>
2024-07-19 15:46:03 +0200

    commit main

--- utils/utils_DL.py
+++ utils/utils_DL.py
@@ -4,31 +4,25 @@ import numpy as np
 from models.model import build_lenet5
 from sklearn.metrics import confusion_matrix
 
-def plot_history(history,metric=None):
-  """
-  Draws in a graph the loss trend over epochs on both training and validation sets. Moreover, if provided, it draws in the same graph also the trend of the given metric.
-  """
-  fig, ax1 = plt.subplots(figsize=(10, 8))
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
 
-  epoch_count=len(history.history['loss'])
-
-  line1,=ax1.plot(range(1,epoch_count+1),history.history['loss'],label='train_loss',color='orange')
-  ax1.plot(range(1,epoch_count+1),history.history['val_loss'],label='val_loss',color = line1.get_color(), linestyle = '--')
-  ax1.set_xlim([1,epoch_count])
-  ax1.set_ylim([0, max(max(history.history['loss']),max(history.history['val_loss']))])
-  ax1.set_ylabel('loss',color = line1.get_color())
-  ax1.tick_params(axis='y', labelcolor=line1.get_color())
-  ax1.set_xlabel('Epochs')
-  _=ax1.legend(loc='lower left')
-
-  if (metric!=None):
-    ax2 = ax1.twinx()
-    line2,=ax2.plot(range(1,epoch_count+1),history.history[metric],label='train_'+metric)
-    ax2.plot(range(1,epoch_count+1),history.history['val_'+metric],label='val_'+metric,color = line2.get_color(), linestyle = '--')
-    ax2.set_ylim([0, max(max(history.history[metric]),max(history.history['val_'+metric]))])
-    ax2.set_ylabel(metric,color=line2.get_color())
-    ax2.tick_params(axis='y', labelcolor=line2.get_color())
-    _=ax2.legend(loc='upper right')
+def build_lenet5(input_shape=(32, 32, 1), output_class_count=10):
+    model = keras.Sequential(
+        [
+            layers.Input(shape=input_shape, name='Input'),
+            layers.Conv2D(filters=6, kernel_size=5, strides=1, activation='tanh', padding='valid', name='C1'),
+            layers.AvgPool2D(pool_size=2, strides=2, name='S2'),
+            layers.Conv2D(filters=16, kernel_size=5, strides=1, activation='tanh', padding='valid', name='C3'),
+            layers.AvgPool2D(pool_size=2, strides=2, name='S4'),
+            layers.Conv2D(filters=120, kernel_size=5, strides=1, activation='tanh', padding='valid', name='C5'),
+            layers.Flatten(),
+            layers.Dense(84, activation='tanh', name='F6'),
+            layers.Dense(units=output_class_count, activation='softmax', name='Output')
+        ]
+    )
+    return model
 
 def train_model(train_x, train_y, val_x, val_y):
   # Build and compile the model
@@ -64,6 +58,32 @@ def evaluate_model(model, test_x, test_y, class_names):
   y_pred = model.predict(test_x)
   y_pred = np.argmax(y_pred, axis=1)
   show_confusion_matrix(confusion_matrix(test_y, y_pred), class_names)
+
+def plot_history(history,metric=None):
+  """
+  Draws in a graph the loss trend over epochs on both training and validation sets. Moreover, if provided, it draws in the same graph also the trend of the given metric.
+  """
+  fig, ax1 = plt.subplots(figsize=(10, 8))
+
+  epoch_count=len(history.history['loss'])
+
+  line1,=ax1.plot(range(1,epoch_count+1),history.history['loss'],label='train_loss',color='orange')
+  ax1.plot(range(1,epoch_count+1),history.history['val_loss'],label='val_loss',color = line1.get_color(), linestyle = '--')
+  ax1.set_xlim([1,epoch_count])
+  ax1.set_ylim([0, max(max(history.history['loss']),max(history.history['val_loss']))])
+  ax1.set_ylabel('loss',color = line1.get_color())
+  ax1.tick_params(axis='y', labelcolor=line1.get_color())
+  ax1.set_xlabel('Epochs')
+  _=ax1.legend(loc='lower left')
+
+  if (metric!=None):
+    ax2 = ax1.twinx()
+    line2,=ax2.plot(range(1,epoch_count+1),history.history[metric],label='train_'+metric)
+    ax2.plot(range(1,epoch_count+1),history.history['val_'+metric],label='val_'+metric,color = line2.get_color(), linestyle = '--')
+    ax2.set_ylim([0, max(max(history.history[metric]),max(history.history['val_'+metric]))])
+    ax2.set_ylabel(metric,color=line2.get_color())
+    ax2.tick_params(axis='y', labelcolor=line2.get_color())
+    _=ax2.legend(loc='upper right')
 
 def show_confusion_matrix(conf_matrix,class_names,figsize=(10,10)):
   fig, ax = plt.subplots(figsize=figsize)
